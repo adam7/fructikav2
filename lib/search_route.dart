ad21f1c1447abe6bda318d671a089d6a3d7901a1
Adam Cooper <adam@sequence7.net>
2019-04-10 18:38:36 +0200

    Pull search out of the AppBar （￣︶￣）↗

--- lib/search_route.dart
+++ lib/search_route.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:fructika/app_drawer.dart';
 import 'package:fructika/database.dart';
@@ -23,124 +22,63 @@ class SearchRouteState extends State<SearchRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: TextField(
-          controller: _search,
-          onSubmitted: (text) {
-            if (text.isEmpty) {
-              foods = List<Food>();
-              setState(() {});
-            } else {
-              DBProvider.db.searchFoods(text).then((result) {
-                foods = result;
-                setState(() {});
-              });
-            }
-          },
-          decoration: InputDecoration(
-              prefixIcon: Icon(Icons.search),
-              hintText: 'Search',
-              suffixIcon: Icon(Icons.search)),
-        )),
-        body: ListView.separated(
-            itemCount: foods.length,
-            separatorBuilder: (BuildContext context, int index) => Divider(),
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => FoodRoute(food: foods[index])));
-                  },
-                  leading: CircleAvatar(
-                      backgroundImage:
-                          Image.asset(foods[index].foodGroupImage).image),
-                  title: Text(
-                    foods[index].description,
-                    style: _biggerFont,
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(
-                      // Add the lines from here...
-                      foods[index].favourite
-                          ? Icons.favorite
-                          : Icons.favorite_border,
-                      color: foods[index].favourite ? Colors.red : null,
-                    ),
-                    onPressed: () {
-                      DBProvider.db.toggleFavourite(foods[index]);
-                      setState(() {});
-                    },
-                  ));
-            }),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () async {
-            // Food rnd = testFoods[math.Random().nextInt(testFoods.length)];
-            // await DBProvider.db.newFood(rnd);
-            // setState(() {});
-          },
+        appBar: AppBar(title: Text("Search Foods")),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              controller: _search,
+              onSubmitted: (text) {
+                if (text.isEmpty) {
+                  foods = List<Food>();
+                  setState(() {});
+                } else {
+                  DBProvider.db.searchFoods(text).then((result) {
+                    foods = result;
+                    setState(() {});
+                  });
+                }
+              },
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Search',
+                  suffixIcon: Icon(Icons.search)),
+            ),
+            Expanded(
+                child: ListView.separated(
+                    itemCount: foods.length,
+                    separatorBuilder: (BuildContext context, int index) =>
+                        Divider(),
+                    itemBuilder: (BuildContext context, int index) {
+                      return _buildFoodTile(foods[index]);
+                    }))
+          ],
         ),
         drawer: AppDrawer());
   }
-}
 
-class FoodsWidget extends StatefulWidget {
-  @override
-  FoodsWidgetState createState() => new FoodsWidgetState();
-}
-
-class FoodsWidgetState extends State<FoodsWidget> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = Set<Food>();
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
+  _buildFoodTile(Food food) {
     return ListTile(
-      leading: CircleAvatar(
-          backgroundImage: Image.asset('images/group_271121.jpg').image),
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        // Add the lines from here...
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => FoodRoute()));
-      },
-      // onTap: () {
-      //   // Add 9 lines from here...
-      //   setState(() {
-      //     if (alreadySaved) {
-      //       _saved.remove(pair);
-      //     } else {
-      //       _saved.add(pair);
-      //     }
-      //   });
-      // },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _buildSuggestions();
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => FoodRoute(food: food)));
+        },
+        leading: CircleAvatar(
+            backgroundImage: Image.asset(food.foodGroupImage).image),
+        title: Text(
+          food.description,
+          style: _biggerFont,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            // Add the lines from here...
+            food.favourite ? Icons.favorite : Icons.favorite_border,
+            color: food.favourite ? Colors.red : null,
+          ),
+          onPressed: () {
+            setState(() {
+              DBProvider.db.toggleFavourite(food);
+            });
+          },
+        ));
   }
 }

557539b586e56547d6d8e47606d7c972fd234275
Adam Cooper <adam@sequence7.net>
2019-04-15 22:17:19 +0200

    Better 🧪s

--- test/app_drawer_test.dart
+++ test/app_drawer_test.dart
@@ -3,10 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fructika/app_drawer.dart';
 
 void main() {
-  testWidgets('App drawer has all the right menu items', (WidgetTester tester) async {
+  testWidgets('App drawer has all the right menu items',
+      (WidgetTester tester) async {
     // Key so we can manipulate the scaffold
     final _scaffoldKey = GlobalKey<ScaffoldState>();
-    
+
     await tester.pumpWidget(
         MaterialApp(home: Scaffold(key: _scaffoldKey, drawer: AppDrawer())));
 
@@ -14,10 +15,12 @@ void main() {
 
     await tester.pump();
 
-    expect(find.text("Food Groups"), findsOneWidget);
-    expect(find.text("Food Search"), findsOneWidget);
-    expect(find.text("Settings"), findsOneWidget);
-    expect(find.text("About"), findsOneWidget);
-    expect(find.text("Favourites"), findsOneWidget);
+    final listTiles = find.byType(ListTile);
+    
+    expect(find.descendant(of: listTiles, matching: find.text("Food Groups")), findsOneWidget);
+    expect(find.descendant(of: listTiles, matching: find.text("Food Search")), findsOneWidget);
+    expect(find.descendant(of: listTiles, matching: find.text("Settings")), findsOneWidget);
+    expect(find.descendant(of: listTiles, matching: find.text("About")), findsOneWidget);
+    expect(find.descendant(of: listTiles, matching: find.text("Favourites")), findsOneWidget);
   });
 }
